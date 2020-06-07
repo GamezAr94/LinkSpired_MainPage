cfe8c961eb43fa14f8a4b3aa26f75196fce21235
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-06-07 12:37:18 -0700

    last changes

--- SCRIPTS/lightHeader.js
+++ SCRIPTS/lightHeader.js
@@ -1,5 +1,6 @@
 
 const header = document.getElementById("header");
+const menuBar = document.getElementById("menu-bar");
 const iluminacion = document.getElementsByClassName("iluminacion");
 window.addEventListener("scroll", function () {
     let offset = window.pageYOffset;
@@ -8,11 +9,14 @@ window.addEventListener("scroll", function () {
     if (windowWidth >= 670) {
         if (offset >= 100) {
             header.className="down";
+            menuBar.className="down";
         } else {
             header.className="up";
+            menuBar.className="up";
         }
     }else{
         header.className="phone";
+        menuBar.className="phone";
     }
 })
 window.addEventListener("resize", function () {
@@ -20,11 +24,14 @@ window.addEventListener("resize", function () {
     let offset = window.pageYOffset;
     if(windowWidth < 670){
         header.className="phone";
+        menuBar.className="phone";
     }else{
         if(offset >= 100){
             header.className="down";
+            menuBar.className="down";
         }else{
             header.className="up";
+            menuBar.className="up";
         }
     }
 })
@@ -33,11 +40,14 @@ window.addEventListener("load", function(){
     let windowWidth = window.innerWidth;
     if(windowWidth < 670){
         header.className="phone";
+        menuBar.className="phone";
     }else{
         if(offset >= 100){
             header.className="down";
+            menuBar.className="down";
         }else{
             header.className="up";
+            menuBar.className="up";
         }
     }
 });
